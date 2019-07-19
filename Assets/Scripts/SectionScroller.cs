cb971e672122b69f6739ef420643d3c760d843fa
1160300527 <15683609851@163.com>
2019-07-19 18:27:53 +0800

    摄像机以及player移动

--- Assets/Scripts/SectionScroller.cs
+++ Assets/Scripts/SectionScroller.cs
@@ -4,13 +4,12 @@
  * Attached to the section so that everything will scroll sideways.
  * The player does not move in this game, the environment does.
  */
-public class SectionScroller : MonoBehaviour
-{
+public class SectionScroller : MonoBehaviour {
     public GameObject[] stuts;
     public GameObject[] obstacles;
     public GameObject block;
     public GameObject flag;
-    private int level=0;
+    private int level = 0;
 
     public int Level { get => level; set => level = value; }
 
@@ -18,72 +17,63 @@ public class SectionScroller : MonoBehaviour
      * Use the Transform component attached to the section game object and
      * translate it based on delta time.
      */
-    private void Update()
-  {
-    transform.Translate(new Vector2(-10, 0) * Time.deltaTime);
-  }
-  private void Awake()
-  {
-      GetGround();
-      GetObstacles();
-      GetFlag();
-  }
-  private void Start()
-  {
-      
-  }
+    private void Update () {
 
-  private void GetGround()
-  {
-      for(int i=0;i<300;i++)
-      {
-            GameObject obj = GameObject.Instantiate(block,transform.position+new Vector3(0.8f*i-100,0,0),new Quaternion(0, 0, 0, 0));
-            obj.transform.parent=transform;
-      }
-  }
+    }
+    private void Awake () {
+        GetGround ();
+        GetObstacles ();
+        GetFlag ();
+    }
+    private void Start () {
 
-  private void GetObstacles()
-  {
-      int distance;
-      switch(Level)
-      {
-        case 1:
-            distance=20;
-            break;
-        case 2:
-            distance=15;
-            break;
-        default:
-            distance=15;
-            break;
-      }
-      int l=Random.Range(25,30);
-      float baseHeight=1.24f;
-      float obsHeight=1.4f;
-      do{
-            int height=Random.Range(0,3);
+    }
+
+    private void GetGround () {
+        for (int i = 0; i < 300; i++) {
+            GameObject obj = GameObject.Instantiate (block, transform.position + new Vector3 (0.8f * i - 100, 0, 0), new Quaternion (0, 0, 0, 0));
+            obj.transform.parent = transform;
+        }
+    }
+
+    private void GetObstacles () {
+        int distance;
+        switch (Level) {
+            case 1:
+                distance = 20;
+                break;
+            case 2:
+                distance = 15;
+                break;
+            default:
+                distance = 15;
+                break;
+        }
+        int l = Random.Range (25, 30);
+        float baseHeight = 1.24f;
+        float obsHeight = 1.4f;
+        do {
+            int height = Random.Range (0, 3);
             GameObject obj;
-            if(height==0){
-                Vector3 position=transform.position+new Vector3(l-100,baseHeight+obsHeight,0);
-                obj = GameObject.Instantiate(obstacles[0],position,new Quaternion(0, 0, 0, 0));
-            }
-            else if(height==1){
-                Vector3 position=transform.position+new Vector3(l-100,baseHeight+0.4f,0);
-                obj = GameObject.Instantiate(obstacles[1],position,new Quaternion(0, 0, 0, 0)); 
-                
-            }
-            else{
-                Vector3 position=transform.position+new Vector3(l-100,baseHeight,0);
-                obj = GameObject.Instantiate(obstacles[2],position,new Quaternion(0, 0, 0, 0)); 
-            }
-            obj.transform.parent=transform;
-            l+=Random.Range(distance,distance+10);
-        }while(l<=200);
-  }
+            if (height == 0) {
+                Vector3 position = transform.position + new Vector3 (l - 100, baseHeight + obsHeight, 0);
+                obj = GameObject.Instantiate (obstacles[0], position, new Quaternion (0, 0, 0, 0));
+            } else if (height == 1) {
+                Vector3 position = transform.position + new Vector3 (l - 100, baseHeight + 0.4f, 0);
+                obj = GameObject.Instantiate (obstacles[1], position, new Quaternion (0, 0, 0, 0));
 
-  private void GetFlag(){
-      GameObject obj = GameObject.Instantiate(flag,transform.position+new Vector3(120,2,0),new Quaternion(0, 0, 0, 0));
-      obj.transform.parent=transform;
-  }
+            } else {
+                Vector3 position = transform.position + new Vector3 (l - 100, baseHeight, 0);
+                obj = GameObject.Instantiate (obstacles[2], position, new Quaternion (0, 0, 0, 0));
+            }
+            obj.transform.parent = transform;
+            l += Random.Range (distance, distance + 10);
+        } while (l <= 200);
+    }
+
+    private void GetFlag () {
+        GameObject obj = GameObject.Instantiate (flag, transform.position + new Vector3 (120, 2, 0), new Quaternion (0, 0, 0, 0));
+        obj.transform.parent = transform;
+    }
 
 }
