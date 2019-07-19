5b42030f63cb284bd7de8a8dca96cfb98fd91f9a
1160300527 <15683609851@163.com>
2019-07-19 09:54:40 +0800

    场景

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,81 +4,69 @@
  * Behaviour to handle keyboard input and also store the player's
  * current health.
  */
-public class PlayerController : MonoBehaviour
-{
-  private Rigidbody2D rigidbody2d;
-  private int health;
-  private bool canJump;
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D rigidbody2d;
+    private int health;
+    private bool canJump;
 
-  /*
-   * Apply initial health and also store the Rigidbody2D reference for
-   * future because GetComponent<T> is relatively expensive.
-   */
-  private void Start()
-  {
-    health = 6;
-    rigidbody2d = GetComponent<Rigidbody2D>();
-  }
-
-  /*
-   * Remove one health unit from player and if health becomes 0, change
-   * scene to the end game scene.
-   */
-  public void Damage()
-  {
-    health -= 1;
-
-    if(health < 1)
-    {
-      Application.LoadLevel("EndGame");
+    /*
+     * Apply initial health and also store the Rigidbody2D reference for
+     * future because GetComponent<T> is relatively expensive.
+     */
+    private void Start () {
+        health = 6;
+        rigidbody2d = GetComponent<Rigidbody2D> ();
     }
-  }
 
-  /*
-   * Accessor for health variable, used by he HUD to display health.
-   */
-  public int GetHealth()
-  {
-    return health;
-  }
+    /*
+     * Remove one health unit from player and if health becomes 0, change
+     * scene to the end game scene.
+     */
+    public void Damage () {
+        health -= 1;
 
-  /*
-   * Poll keyboard for when the up arrow is pressed. If the player can jump
-   * (is on the ground) then add force to the cached Rigidbody2D component.
-   * Finally unset the canJump flag so the player has to wait to land before
-   * another jump can be triggered.
-   */
-  private void Update()
-  {
-    if(canJump == true)
-    {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            rigidbody2d.AddForce(new Vector2(0, 500));
-            canJump = false;
-        }
-        else if(Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidbody2d.AddForce(new Vector2(0, 600));
-            canJump = false;
-        }
-        else if(Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            transform.localScale=new Vector3(1f,0.6f,0.5f);
-        }
-        else if(Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            transform.localScale=new Vector3(1.0f,1.0f,1.0f);   
+        if (health < 1) {
+            Application.LoadLevel ("EndGame");
         }
     }
-  }
 
-  /*
-   * If the player has collided with the ground, set the canJump flag so that
-   * the player can trigger another jump.
-   */
-  private void OnCollisionEnter2D(Collision2D other)
-  {
-    canJump = true;
-  }
+    /*
+     * Accessor for health variable, used by he HUD to display health.
+     */
+    public int GetHealth () {
+        return health;
+    }
+
+    /*
+     * Poll keyboard for when the up arrow is pressed. If the player can jump
+     * (is on the ground) then add force to the cached Rigidbody2D component.
+     * Finally unset the canJump flag so the player has to wait to land before
+     * another jump can be triggered.
+     */
+    private void Update () {
+        if (canJump == true) {
+            if (Input.GetKeyDown (KeyCode.UpArrow)) {
+                rigidbody2d.AddForce (new Vector2 (0, 500));
+                canJump = false;
+            } else if (Input.GetKeyDown (KeyCode.Space)) {
+                rigidbody2d.AddForce (new Vector2 (0, 600));
+                canJump = false;
+            } else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+                transform.localScale = new Vector3 (1f, 0.6f, 0.5f);
+                canJump = false;
+            }
+        }
+        if (Input.GetKeyUp (KeyCode.DownArrow)) {
+            transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+            canJump = true;
+        }
+    }
+
+    /*
+     * If the player has collided with the ground, set the canJump flag so that
+     * the player can trigger another jump.
+     */
+    private void OnCollisionEnter2D (Collision2D other) {
+        canJump = true;
+    }
 }
